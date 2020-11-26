46857837329879fe8a5dde11f3b16db5f7ae70a3
Suma <sum@dk.com>
2020-11-26 11:03:44 +0000

    Circle Exercise

---
+++ src/main/java/org/oop/Circle.java
@@ -0,0 +1,26 @@
+package org.oop;
+
+public class Circle {
+    private final String color = "Red";
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    // method
+    public double getArea() {
+        return (Math.PI * radius * radius);
+
+    }
+
+}
+
+
---
+++ src/test/java/org/oop/CircleTest.java
@@ -0,0 +1,23 @@
+package org.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CircleTest {
+    Circle circleNoArg = new Circle();
+    Circle circleArg = new Circle(10.0);
+
+    @Test
+    void getAreaNoArg() {
+        assertEquals(Math.PI, circleNoArg.getArea());
+
+    }
+
+    @Test
+    void getAreaArg() {
+        var expectedArea = (Math.PI * 10 * 10);
+        assertEquals(expectedArea, circleArg.getArea());
+
+    }
+}
