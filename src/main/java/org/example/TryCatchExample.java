5bb13a69f0db794e1e5f5d5d90cb9ec749fa4a0d
Suma <sum@dk.com>
2020-11-27 11:38:55 +0000

    TryCatch with Logger

--- src/main/java/org/example/TryCatchExample.java
+++ src/main/java/org/example/TryCatchExample.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TryCatchExample {
+    public static final Logger LOGGER =Logger.getAnonymousLogger();
     //Method 1 String noTryCatch(Strings[]  strings,int index)
     String noTryCatch(String[] strings,int index) {
         return strings[index];
@@ -10,6 +14,7 @@ public class TryCatchExample {
         try {
            return strings[index];
         }catch (ArrayIndexOutOfBoundsException e) {
+            LOGGER.log(Level.WARNING,"ArrayIndexOutBoundException with Index : " + index);
 
         return "An exception was caught";
     }
